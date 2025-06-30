24532fdc31f792febd2d4384bd0f56c78463a187
Mrdarkiry <benjaleivamartinez56@gmail.com>
2025-06-30 10:29:29 -0400

    Create funciones.py

---
+++ funciones.py
@@ -0,0 +1,7 @@
+def saludo(nombre, mensaje="¡Bienvenido!"):
+    print(f"Hola, {nombre}. {mensaje}")
+
+saludo("Carlos")  # Salida: Hola, Carlos. ¡Bienvenido!
+saludo("Ana", "Qué bueno verte!")  # Salida: Hola, Ana. Qué bueno verte!
+saludo("Benjamin")
+saludo("Ignacion", "Como estas")
